c1486822fc6561fc5b1a026e1029ffa957866068
Tatsuya_Sonobe <Tatsuya_Sonobe@hm.honda.co.jp>
2021-07-02 14:04:17 +0900

    [update]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react"
 import { firebaseStore } from "./firebase";
+import { AppBar, Toolbar, Typography, TableContainer, Paper, Table, TableHead, TableCell, TableRow, TableBody } from "@material-ui/core";
+
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,15 +24,39 @@ function App() {
     setLoading(false);
   }, []);
 
-
   return (
-    <div style={{ textAlign:"center", verticalAlign:"middle" }}>
-      {!loading && time.map((item) => (
-        <div key={item.class}>
-          class{item.class}{" "}{item.hour}時{item.minute}分
-        </div>
-      ))}
+    <div>    
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Time Schedule
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ margin: 10 }}>
+        <TableContainer component={Paper} >
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Class</TableCell>
+                <TableCell align="right">Time</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!loading && time.map((row) => (
+                <TableRow key={row.class}>
+                  <TableCell component="th" scope="row">
+                    {row.class}
+                  </TableCell>
+                  <TableCell align="right">{row.hour}時{row.minute}分</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
+
   );
 }
 
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,5 @@
 import firebase from 'firebase';
 
-// const env = process.env;
-
-// export const firebaseConfig = {
-//     apiKey: env.REACT_APP_API_KEY,
-//     authDomain: env.REACT_APP_AUTH_DOMAIN,
-//     databaseURL: env.REACT_APP_DATABASE_URL,
-//     projectId: env.REACT_APP_PROJECT_ID,
-//     storageBucket: env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: env.REACT_APP_APP_ID,
-//     measurementId: env.REACT_APP_MEASUREMENT_ID,
-// };
-
 export const firebaseConfig = {
     apiKey: "AIzaSyABwCy81BdBJwkFiEd3831S1w7kEND-AFM",
     authDomain: "time-schedule-58a95.firebaseapp.com",
